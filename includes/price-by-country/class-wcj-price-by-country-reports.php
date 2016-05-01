34df7e62816d31002aa011eced1e9a95117a38b7
algoritmika <info@algoritmika.com>
2016-05-02 00:15:03 +0300

    v2.4.8-dev

--- includes/price-by-country/class-wcj-price-by-country-reports.php
+++ includes/price-by-country/class-wcj-price-by-country-reports.php
@@ -4,7 +4,7 @@
  *
  * The WooCommerce Jetpack Price By Country Reports class.
  *
- * @version  2.2.0
+ * @version  2.4.8
  * @author   Algoritmika Ltd.
  */
 
@@ -14,19 +14,20 @@ if ( ! class_exists( 'WCJ_Price_By_Country_Reports' ) ) :
 
 class WCJ_Price_By_Country_Reports {
 
-    /**
-     * Constructor.
-     */
-    function __construct() {
-
+	/**
+	 * Constructor.
+	 */
+	function __construct() {
 		add_filter( 'woocommerce_reports_get_order_report_data_args', array( $this, 'filter_reports'),                     PHP_INT_MAX, 1 );
 		add_filter( 'woocommerce_currency_symbol',                    array( $this, 'change_currency_symbol_reports'),     PHP_INT_MAX, 2 );
 		add_action( 'admin_bar_menu',                                 array( $this, 'add_reports_currency_to_admin_bar' ), PHP_INT_MAX );
-    }
+	}
 
-    /**
-     * add_reports_currency_to_admin_bar.
-     */
+	/**
+	 * add_reports_currency_to_admin_bar.
+	 *
+	 * @version 2.4.8
+	 */
 	function add_reports_currency_to_admin_bar( $wp_admin_bar ) {
 
 		if ( isset( $_GET['page'] ) && 'wc-reports' === $_GET['page'] ) {
@@ -45,13 +46,15 @@ class WCJ_Price_By_Country_Reports {
 			$currency_symbols = array();
 			$currency_symbols[ $the_current_code ] = '';
 			$currency_symbols[ get_woocommerce_currency() ] = '';
-			for ( $i = 1; $i <= apply_filters( 'wcj_get_option_filter', 1, get_option( 'wcj_price_by_country_total_groups_number', 1 ) ); $i++ ) {
-				$currency_symbols[ get_option( 'wcj_price_by_country_exchange_rate_currency_group_' . $i ) ] = '';
+			if ( wcj_is_module_enabled( 'price_by_country' ) ) {
+				for ( $i = 1; $i <= apply_filters( 'wcj_get_option_filter', 1, get_option( 'wcj_price_by_country_total_groups_number', 1 ) ); $i++ ) {
+					$currency_symbols[ get_option( 'wcj_price_by_country_exchange_rate_currency_group_' . $i ) ] = '';
+				}
 			}
 			$currency_symbols['merge'] = '';
-			$this->reports_currency_symbols = $currency_symbols;
+			$reports_currency_symbols = $currency_symbols;
 
-			foreach ( $this->reports_currency_symbols as $code => $symbol ) {
+			foreach ( $reports_currency_symbols as $code => $symbol ) {
 				//if ( $code === $the_current_code )
 				//	continue;
 				$args = array(
@@ -74,7 +77,6 @@ class WCJ_Price_By_Country_Reports {
 		if ( isset( $_GET['page'] ) && 'wc-reports' === $_GET['page'] ) {
 			if ( isset( $_GET['currency'] ) ) {
 				return ( 'merge' === $_GET['currency'] ) ? '' : wcj_get_currency_symbol( $_GET['currency'] );
-				//return wcj_get_currency_symbol( $_GET['currency'] );
 			}
 		}
 		return $currency_symbol;
@@ -84,13 +86,14 @@ class WCJ_Price_By_Country_Reports {
 	 * filter_reports.
 	 */
 	function filter_reports( $args ) {
-		//if ( ! isset( $_GET['currency'] ) ) return $args;
-		if ( isset( $_GET['currency'] ) && 'merge' === $_GET['currency'] ) return $args;
+//		if ( ! isset( $_GET['currency'] ) ) return $args;
+		if ( isset( $_GET['currency'] ) && 'merge' === $_GET['currency'] ) {
+			return $args;
+		}
 		$args['where_meta'] = array(
 			array(
-				'meta_key' 	 => '_order_currency',
+				'meta_key'   => '_order_currency',
 				'meta_value' => isset( $_GET['currency'] ) ? $_GET['currency'] : get_woocommerce_currency(),
-				//'meta_value' => $_GET['currency'],
 				'operator'   => '=',
 			),
 		);
