bc39a4a81cbb271385483bcd3aba43ec9a11d4e8
algoritmika <info@algoritmika.com>
2016-05-31 19:39:49 +0300

    v2.5.2-dev

--- includes/shipping/class-wc-shipping-wcj-custom.php
+++ includes/shipping/class-wc-shipping-wcj-custom.php
@@ -4,7 +4,7 @@
  *
  * The WooCommerce Jetpack Custom Shipping class.
  *
- * @version 2.4.8
+ * @version 2.5.2
  * @since   2.4.8
  * @author  Algoritmika Ltd.
  */
@@ -31,8 +31,9 @@ if ( ! function_exists( 'init_wc_shipping_wcj_custom_class' ) ) {
 				/**
 				 * Init settings
 				 *
-				 * @access public
-				 * @return void
+				 * @version 2.5.2
+				 * @access  public
+				 * @return  void
 				 */
 				function init( $id_count ) {
 
@@ -49,6 +50,13 @@ if ( ! function_exists( 'init_wc_shipping_wcj_custom_class' ) ) {
 					$this->title    = $this->get_option( 'title' );
 					$this->cost     = $this->get_option( 'cost' );
 					$this->type     = $this->get_option( 'type' );
+					$this->weight_table_total_rows = $this->get_option( 'weight_table_total_rows' );
+					for ( $i = 1; $i <= $this->weight_table_total_rows; $i++ ) {
+						$option_name = 'weight_table_weight_row_' . $i;
+						$this->$option_name = $this->get_option( $option_name );
+						$option_name = 'weight_table_cost_row_' . $i;
+						$this->$option_name = $this->get_option( $option_name );
+					}
 
 					// Save settings in admin
 					add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
@@ -56,6 +64,8 @@ if ( ! function_exists( 'init_wc_shipping_wcj_custom_class' ) ) {
 
 				/**
 				 * Initialise Settings Form Fields
+				 *
+				 * @version 2.5.2
 				 */
 				function init_form_fields() {
 					$this->form_fields = array(
@@ -79,9 +89,10 @@ if ( ! function_exists( 'init_wc_shipping_wcj_custom_class' ) ) {
 							'default'     => 'flat_rate',
 							'desc_tip'    => true,
 							'options'     => array(
-								'flat_rate'              => __( 'Flat Rate', 'woocommerce-jetpack' ),
-								'by_total_cart_weight'   => __( 'By Total Cart Weight', 'woocommerce-jetpack' ),
-								'by_total_cart_quantity' => __( 'By Total Cart Quantity', 'woocommerce-jetpack' ),
+								'flat_rate'                  => __( 'Flat Rate', 'woocommerce-jetpack' ),
+								'by_total_cart_weight'       => __( 'By Total Cart Weight', 'woocommerce-jetpack' ),
+								'by_total_cart_weight_table' => __( 'By Total Cart Weight Table', 'woocommerce-jetpack' ),
+								'by_total_cart_quantity'     => __( 'By Total Cart Quantity', 'woocommerce-jetpack' ),
 							),
 						),
 						'cost' => array(
@@ -92,15 +103,62 @@ if ( ! function_exists( 'init_wc_shipping_wcj_custom_class' ) ) {
 							'desc_tip'    => true,
 							'custom_attributes' => array( 'step' => '0.000001', 'min'  => '0', ),
 						),
+						'weight_table_total_rows' => array(
+							'title'       => __( 'Weight Table Total Rows', 'woocommerce' ),
+							'type'        => 'number',
+							'default'     => 0,
+							'desc_tip'    => true,
+							'custom_attributes' => array( 'min'  => '0', ),
+						),
 					);
+					for ( $i = 1; $i <= $this->get_option( 'weight_table_total_rows' ); $i++ ) {
+						$this->form_fields = array_merge( $this->form_fields, array(
+							'weight_table_weight_row_' . $i => array(
+								'title'       => __( 'Max Weight', 'woocommerce' ) . ' #' . $i,
+								'type'        => 'number',
+								'default'     => 0,
+								'desc_tip'    => true,
+								'custom_attributes' => array( 'step' => '0.000001', 'min'  => '0', ),
+							),
+							'weight_table_cost_row_' . $i => array(
+								'title'       => __( 'Cost', 'woocommerce' ) . ' #' . $i,
+								'type'        => 'number',
+								'default'     => 0,
+								'desc_tip'    => true,
+								'custom_attributes' => array( 'step' => '0.000001', 'min'  => '0', ),
+							),
+						) );
+					}
+				}
+
+				/**
+				 * calculate_shipping_by_weight_table.
+				 *
+				 * @version 2.5.2
+				 * @since   2.5.2
+				 */
+				function calculate_shipping_by_weight_table( $weight ) {
+					if ( 0 == $this->weight_table_total_rows ) {
+						return $this->cost * $weight; // fallback
+					}
+					$option_name_weight = $option_name_cost = '';
+					for ( $i = 1; $i <= $this->weight_table_total_rows; $i++ ) {
+						$option_name_weight = 'weight_table_weight_row_' . $i;
+						$option_name_cost = 'weight_table_cost_row_' . $i;
+						if ( $weight <= $this->$option_name_weight ) {
+							return $this->$option_name_cost;
+						}
+					}
+					return $this->$option_name_cost; // fallback - last row
 				}
 
 				/**
 				 * calculate_shipping function.
 				 *
-				 * @access public
-				 * @param mixed $package
-				 * @return void
+				 * @version 2.5.2
+				 * @access  public
+				 * @param   mixed $package
+				 * @return  void
 				 */
 				function calculate_shipping( $package ) {
 					switch ( $this->type ) {
@@ -113,6 +171,9 @@ if ( ! function_exists( 'init_wc_shipping_wcj_custom_class' ) ) {
 							break;
 						case 'by_total_cart_weight':
 							$cost = $this->cost * WC()->cart->get_cart_contents_weight();
+							break;
+						case 'by_total_cart_weight_table':
+							$cost = $this->calculate_shipping_by_weight_table( WC()->cart->get_cart_contents_weight() );
 							break;
 						default: // 'flat_rate'
 							$cost = $this->cost;
