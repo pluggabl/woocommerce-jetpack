226fe03a437382e356eb054672ba425704be61d5
algoritmika <info@algoritmika.com>
2016-06-04 02:05:04 +0300

    v2.5.2-dev

--- woocommerce-jetpack.php
+++ woocommerce-jetpack.php
@@ -39,7 +39,7 @@ final class WC_Jetpack {
 	 * @var   string
 	 * @since 2.4.7
 	 */
-	public $version = '2.5.2-dev-201606011936';
+	public $version = '2.5.2-dev-201606040204';
 
 	/**
 	 * @var WC_Jetpack The single instance of the class
