7ba971251b976c33a9a4f7cfef89dd098e7850f2
algoritmika <info@algoritmika.com>
2017-04-10 17:46:06 +0300

    v2.6.1-dev: Minor changes.

--- includes/admin/class-wc-settings-jetpack.php
+++ includes/admin/class-wc-settings-jetpack.php
@@ -588,7 +588,7 @@ class WC_Settings_Jetpack extends WC_Settings_Page {
 						'product_addons',
 						'currency_per_product',
 						'reports',
-						'sku', // "Allow Duplicate SKUs" - seem not to have any effect; "Add SKU to Customer Emails" - didn't check.
+						'sku',                       // "Allow Duplicate SKUs" - seem not to have any effect; "Add SKU to Customer Emails" - didn't check.
 					);
 					$is_wc_3_compatible = ( in_array( $section, $wc_3_compatible_modules ) ?
 						'<span title="' . __( 'WooCommerce v3.x.x compatible', 'woocommerce-jetpack' ) . '" style="color:green;font-weight: bold;margin-right: 5px;padding-left: 5px;">&#9745; </span>' : '' );
