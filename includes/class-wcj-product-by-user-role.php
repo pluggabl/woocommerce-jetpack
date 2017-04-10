ab6ab2163ab487723e72c097182578ed762bd0d8
algoritmika <info@algoritmika.com>
2017-04-10 12:02:39 +0300

    v2.6.1-dev: Dev - WooCommerce v3.0.0 compatibility - PRODUCTS - Product Visibility by User Role - `wcj_get_product_id_or_variation_parent_id()`.

--- includes/class-wcj-product-by-user-role.php
+++ includes/class-wcj-product-by-user-role.php
@@ -4,7 +4,7 @@
  *
  * The WooCommerce Jetpack Product by User Role class.
  *
- * @version 2.6.0
+ * @version 2.6.1
  * @since   2.5.5
  * @author  Algoritmika Ltd.
  */
@@ -90,11 +90,11 @@ class WCJ_Product_By_User_Role extends WCJ_Module {
 	/**
 	 * product_by_user_role_purchasable.
 	 *
-	 * @version 2.6.0
+	 * @version 2.6.1
 	 * @since   2.6.0
 	 */
 	function product_by_user_role_purchasable( $purchasable, $_product ) {
-		return $this->product_by_user_role_visibility( $purchasable, $_product->id );
+		return $this->product_by_user_role_visibility( $purchasable, wcj_get_product_id_or_variation_parent_id( $_product ) );
 	}
 
 	/**
--- woocommerce-jetpack.php
+++ woocommerce-jetpack.php
@@ -59,7 +59,7 @@ final class WC_Jetpack {
 	 * @var   string
 	 * @since 2.4.7
 	 */
-	public $version = '2.6.1-dev-201704091223';
+	public $version = '2.6.1-dev-201704101202';
 
 	/**
 	 * @var WC_Jetpack The single instance of the class
