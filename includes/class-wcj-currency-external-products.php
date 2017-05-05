577aeaaa3135d8264fe26f04a512dd875820be34
algoritmika <info@algoritmika.com>
2017-05-06 01:04:33 +0300

    v2.8.0-dev: Dev - Code refactoring - Settings from file - PRICES & CURRENCIES - Currency for External Products.

--- includes/class-wcj-currency-external-products.php
+++ includes/class-wcj-currency-external-products.php
@@ -1,10 +1,8 @@
 <?php
 /**
- * WooCommerce Jetpack Currency for External Products
+ * Booster for WooCommerce - Module - Currency for External Products
  *
- * The WooCommerce Jetpack Currency for External Products class.
- *
- * @version 2.7.0
+ * @version 2.8.0
  * @author  Algoritmika Ltd.
  */
 
@@ -17,14 +15,14 @@ class WCJ_Currency_External_Products extends WCJ_Module {
 	/**
 	 * Constructor.
 	 *
-	 * @version 2.7.0
+	 * @version 2.8.0
 	 */
 	function __construct() {
 
 		$this->id         = 'currency_external_products';
 		$this->short_desc = __( 'Currency for External Products', 'woocommerce-jetpack' );
 		$this->desc       = __( 'Set different currency for external WooCommerce products.', 'woocommerce-jetpack' );
-		$this->link       = 'http://booster.io/features/woocommerce-currency-for-external-products/';
+		$this->link_slug  = 'woocommerce-currency-for-external-products';
 		parent::__construct();
 
 		if ( $this->is_enabled() ) {
@@ -62,34 +60,6 @@ class WCJ_Currency_External_Products extends WCJ_Module {
 		return $currency_symbol;
 	}
 
-	/**
-	 * get_settings.
-	 *
-	 * @version 2.4.4
-	 */
-	function get_settings() {
-		$settings = array(
-			array(
-				'title'    => __( 'Currency for External Products Options', 'woocommerce-jetpack' ),
-				'type'     => 'title',
-				'desc'     => '',
-				'id'       => 'wcj_currency_external_products_options',
-			),
-			array(
-				'title'    => __( 'Currency', 'woocommerce-jetpack' ),
-				'desc_tip' => __( 'Set currency for all external products.', 'woocommerce-jetpack' ),
-				'id'       => 'wcj_currency_external_products_symbol', // mislabeled, should be 'wcj_currency_external_products_code'
-				'default'  => 'EUR',
-				'type'     => 'select',
-				'options'  => wcj_get_currencies_names_and_symbols(),
-			),
-			array(
-				'type'     => 'sectionend',
-				'id'       => 'wcj_currency_external_products_options',
-			),
-		);
-		return $this->add_standard_settings( $settings );
-	}
 }
 
 endif;
---
+++ includes/settings/wcj-settings-currency-external-products.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Booster for WooCommerce - Settings - Currency for External Products
+ *
+ * @version 2.8.0
+ * @since   2.8.0
+ * @author  Algoritmika Ltd.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+return array(
+	array(
+		'title'    => __( 'Currency for External Products Options', 'woocommerce-jetpack' ),
+		'type'     => 'title',
+		'desc'     => '',
+		'id'       => 'wcj_currency_external_products_options',
+	),
+	array(
+		'title'    => __( 'Currency', 'woocommerce-jetpack' ),
+		'desc_tip' => __( 'Set currency for all external products.', 'woocommerce-jetpack' ),
+		'id'       => 'wcj_currency_external_products_symbol', // mislabeled, should be 'wcj_currency_external_products_code'
+		'default'  => 'EUR',
+		'type'     => 'select',
+		'options'  => wcj_get_currencies_names_and_symbols(),
+	),
+	array(
+		'type'     => 'sectionend',
+		'id'       => 'wcj_currency_external_products_options',
+	),
+);
