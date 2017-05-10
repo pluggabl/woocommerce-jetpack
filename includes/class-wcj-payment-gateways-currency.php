78041d25d80e78163ea3e592f6579a0b687ca08f
algoritmika <info@algoritmika.com>
2017-05-10 17:57:19 +0300

    v2.8.0-dev: Dev - Code refactoring - Settings from file - PAYMENT GATEWAYS - Gateways Currency.

--- includes/class-wcj-payment-gateways-currency.php
+++ includes/class-wcj-payment-gateways-currency.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Booster for WooCommerce - Module - Payment Gateways Currency
+ * Booster for WooCommerce - Module - Gateways Currency
  *
- * @version 2.7.0
+ * @version 2.8.0
  * @since   2.3.0
  * @author  Algoritmika Ltd.
  */
@@ -16,7 +16,7 @@ class WCJ_Payment_Gateways_Currency extends WCJ_Module {
 	/**
 	 * Constructor.
 	 *
-	 * @version 2.5.0
+	 * @version 2.8.0
 	 */
 	function __construct() {
 
@@ -26,10 +26,8 @@ class WCJ_Payment_Gateways_Currency extends WCJ_Module {
 		$this->link_slug  = 'woocommerce-payment-gateways-currency';
 		parent::__construct();
 
-		add_action( 'init', array( $this, 'add_settings_hook' ) );
-
 		if ( $this->is_enabled() ) {
-			/* add_action( 'init', array( $this, 'add_hooks' ) ); */
+//			add_action( 'init', array( $this, 'add_hooks' ) );
 			$this->add_hooks();
 			if ( is_admin() ) {
 				include_once( 'reports/class-wcj-currency-reports.php' );
@@ -94,7 +92,7 @@ class WCJ_Payment_Gateways_Currency extends WCJ_Module {
 	 * @version 2.3.5
 	 */
 	function is_cart_or_checkout() {
-		//if ( wcj_is_frontend() ) {
+//		if ( wcj_is_frontend() ) {
 		if ( ! is_admin() ) {
 			if ( is_cart() || is_checkout() ) {
 				return true;
@@ -165,14 +163,16 @@ class WCJ_Payment_Gateways_Currency extends WCJ_Module {
 		if ( $this->is_cart_or_checkout() ) {
 			global $woocommerce;
 			$current_gateway = $woocommerce->session->chosen_payment_method;
-			/* $available_gateways = WC()->payment_gateways->get_available_payment_gateways();
+			/*
+			$available_gateways = WC()->payment_gateways->get_available_payment_gateways();
 			if ( ! array_key_exists( $current_gateway, $available_gateways ) ) {
 				$current_gateway = get_option( 'woocommerce_default_gateway', '' );
 				if ( '' == $current_gateway ) {
 					$current_gateway = current( $available_gateways );
 					$current_gateway = isset( $current_gateway->id ) ? $current_gateway->id : '';
 				}
-			} */
+			}
+			*/
 			if ( '' != $current_gateway ) {
 				$gateway_currency = get_option( 'wcj_gateways_currency_' . $current_gateway );
 				if ( 'no_changes' != $gateway_currency ) {
@@ -200,90 +200,6 @@ class WCJ_Payment_Gateways_Currency extends WCJ_Module {
 		wp_enqueue_script( 'wcj-payment-gateways-checkout' );
 	}
 
-	/**
-	 * add_settings.
-	 *
-	 * @version 2.7.0
-	 */
-	function add_settings( $settings ) {
-
-		$settings[] = array(
-			'title' => __( 'Payment Gateways Currency Options', 'woocommerce-jetpack' ),
-			'type'  => 'title',
-			'desc'  => __( 'This section lets you set different currency for each payment gateway.', 'woocommerce-jetpack' ),
-			'id'    => 'wcj_payment_gateways_currency_options',
-		);
-
-		$currency_from = get_woocommerce_currency();
-
-		global $woocommerce;
-		$available_gateways = $woocommerce->payment_gateways->payment_gateways();
-		foreach ( $available_gateways as $key => $gateway ) {
-
-			$currency_to = get_option( 'wcj_gateways_currency_' . $key, get_woocommerce_currency() );
-			$custom_attributes = array(
-				'currency_from'        => $currency_from,
-				'currency_to'          => $currency_to,
-				'multiply_by_field_id' => 'wcj_gateways_currency_exchange_rate_' . $key,
-			);
-			if ( $currency_from == $currency_to ) {
-				$custom_attributes['disabled'] = 'disabled';
-			}
-			if ( 'no_changes' == $currency_to ) {
-				$custom_attributes['disabled'] = 'disabled';
-				$currency_to = $currency_from;
-			}
-
-			$settings = array_merge( $settings, array(
-
-				array(
-					'title'     => $gateway->title,
-//					'desc'      => __( 'currency', 'woocommerce-jetpack' ),
-					'id'        => 'wcj_gateways_currency_' . $key,
-					'default'   => 'no_changes',//get_woocommerce_currency(),
-					'type'      => 'select',
-					'options'   => array_merge( array( 'no_changes' => __( 'No changes', 'woocommerce-jetpack' ) ), wcj_get_currencies_names_and_symbols() ),
-				),
-
-				array(
-					'title'                    => '',
-//					'desc'                     => __( 'exchange rate', 'woocommerce-jetpack' ) . ' ' . $currency_from . ' / ' . $currency_to,
-					'id'                       => 'wcj_gateways_currency_exchange_rate_' . $key,
-					'default'                  => 1,
-					'type'                     => 'exchange_rate',
-					'custom_attributes'        => array( 'step' => '0.000001', 'min'  => '0', ),
-					'custom_attributes_button' => $custom_attributes,
-					'css'                      => 'width:100px;',
-					'value'                    => $currency_from . '/' . $currency_to,
-				),
-
-			) );
-		}
-
-		$settings[] = array(
-			'title'    => __( 'Exchange Rates Updates', 'woocommerce-jetpack' ),
-			'id'       => 'wcj_gateways_currency_exchange_rate_update_auto',
-			'default'  => 'manual',
-			'type'     => 'select',
-			'options'  => array(
-				'manual' => __( 'Enter Rates Manually', 'woocommerce-jetpack' ),
-				'auto'   => __( 'Automatically via Currency Exchange Rates module', 'woocommerce-jetpack' ),
-			),
-			'desc'     => ( '' == apply_filters( 'booster_get_message', '', 'desc' ) ) ?
-				__( 'Visit', 'woocommerce-jetpack' ) . ' <a href="' . admin_url( 'admin.php?page=wc-settings&tab=jetpack&wcj-cat=prices_and_currencies&section=currency_exchange_rates' ) . '">' . __( 'Currency Exchange Rates module', 'woocommerce-jetpack' ) . '</a>'
-				:
-				apply_filters( 'booster_get_message', '', 'desc' ),
-			'custom_attributes'
-			           => apply_filters( 'booster_get_message', '', 'disabled' ),
-		);
-
-		$settings[] = array(
-			'type'  => 'sectionend',
-			'id'    => 'wcj_payment_gateways_currency_options',
-		);
-
-		return $settings;
-	}
 }
 
 endif;
