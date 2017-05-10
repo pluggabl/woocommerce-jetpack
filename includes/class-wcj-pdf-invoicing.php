f5fbacd030504be104bccb83e0444ffeccd526b2
algoritmika <info@algoritmika.com>
2017-05-10 19:13:22 +0300

    v2.8.0-dev: Dev - Code refactoring - Settings from file - PDF INVOICING & PACKING SLIPS - General.

--- includes/class-wcj-pdf-invoicing.php
+++ includes/class-wcj-pdf-invoicing.php
@@ -2,7 +2,7 @@
 /**
  * Booster for WooCommerce - Module - PDF Invoicing
  *
- * @version 2.7.0
+ * @version 2.8.0
  * @author  Algoritmika Ltd.
  */
 
@@ -15,9 +15,9 @@ class WCJ_PDF_Invoicing extends WCJ_Module {
 	/**
 	 * Constructor.
 	 *
-	 * @version 2.7.0
+	 * @version 2.8.0
 	 */
-	public function __construct() {
+	function __construct() {
 
 		$this->id            = 'pdf_invoicing';
 		$this->short_desc    = __( 'PDF Invoicing', 'woocommerce-jetpack' );
@@ -330,81 +330,6 @@ class WCJ_PDF_Invoicing extends WCJ_Module {
 		die();
 	}
 
-	/**
-	 * get_settings.
-	 *
-	 * @version 2.7.0
-	 */
-	function get_settings() {
-
-		$settings = array(
-			array(
-				'title' => __( 'PDF Invoicing General Options', 'woocommerce-jetpack' ),
-				'type'  => 'title',
-				'id'    => 'wcj_pdf_invoicing_options',
-			),
-		);
-
-		// Hooks Array
-		$create_on_array = array();
-		$create_on_array['disabled'] = __( 'Disabled', 'woocommerce-jetpack' );
-		$create_on_array['woocommerce_new_order'] = __( 'Create on New Order', 'woocommerce-jetpack' );
-		$order_statuses = wcj_get_order_statuses( true );
-		foreach ( $order_statuses as $status => $desc ) {
-			$create_on_array[ 'woocommerce_order_status_' . $status ] = __( 'Create on Order Status', 'woocommerce-jetpack' ) . ' ' . $desc;
-		}
-		$create_on_array['manual'] = __( 'Manual Only', 'woocommerce-jetpack' );
-
-		// Settings
-		$invoice_types = wcj_get_invoice_types();
-		foreach ( $invoice_types as $k => $invoice_type ) {
-
-			if ( 'custom_doc' === $invoice_type['id'] ) {
-				$settings[] = array(
-					'title'    => __( 'Number of Custom Documents', 'woocommerce-jetpack' ),
-					'desc_tip' => __( 'Save changes after setting this number.', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_custom_doc_total_number',
-					'default'  => 1,
-					'type'     => 'custom_number',
-					'custom_attributes' => array( 'min' => '1' ),
-				);
-			}
-
-			$settings[] = array(
-				'title'    => $invoice_type['title'],
-				'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_create_on',
-				'default'  => 'disabled',
-				'type'     => 'select',
-				'class'    => 'chosen_select',
-				'options'  => $create_on_array,
-				'desc'     => ( 0 === $k ) ? '' : apply_filters( 'booster_get_message', '', 'desc' ),
-				'custom_attributes' => ( 0 === $k ) ? '' : apply_filters( 'booster_get_message', '', 'disabled' ),
-			);
-
-			$settings[] = array(
-				'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_skip_zero_total',
-				'default'  => 'no',
-				'type'     => 'checkbox',
-				'desc'     => __( 'Do not create if order total equals zero', 'woocommerce-jetpack' ),// . ( ( 0 === $k ) ? '' : ' ' . apply_filters( 'booster_get_message', '', 'desc' ) ),
-				'custom_attributes' => ( 0 === $k ) ? '' : apply_filters( 'booster_get_message', '', 'disabled' ),
-			);
-		}
-
-		$settings[] = array(
-			'title'     => __( 'Hide Disabled Docs Settings', 'woocommerce-jetpack' ),
-			'desc'      => __( 'Hide', 'woocommerce-jetpack' ),
-			'id'        => 'wcj_invoicing_hide_disabled_docs_settings',
-			'default'   => 'no',
-			'type'      => 'checkbox',
-		);
-
-		$settings[] = array(
-			'type'      => 'sectionend',
-			'id'        => 'wcj_pdf_invoicing_options',
-		);
-
-		return $this->add_standard_settings( $settings );
-	}
 }
 
 endif;
--- readme.txt
+++ readme.txt
@@ -239,6 +239,7 @@ You can see the differences between versions in this [table](https://booster.io/
 	* Dev - Code refactoring - Settings from file - SHIPPING & ORDERS - Orders.
 	* Dev - Code refactoring - Settings from file - SHIPPING & ORDERS - Shipping.
 	* Dev - Code refactoring - Settings from file - SHIPPING & ORDERS - Shipping Calculator.
+	* Dev - Code refactoring - Settings from file - PDF INVOICING & PACKING SLIPS - General.
 	* Dev - Code refactoring - Settings from file - EMAILS & MISC. - Admin Tools.
 	* Dev - Code refactoring - Settings from file - EMAILS & MISC. - Custom CSS.
 	* Dev - Code refactoring - Settings from file - EMAILS & MISC. - Emails.
