6c463758160a67b6a309809296f53e1683d5d44d
algoritmika <info@algoritmika.com>
2017-05-10 18:19:01 +0300

    v2.8.0-dev: Clean up.

--- includes/settings/wcj-settings-payment-gateways-icons.php
+++ includes/settings/wcj-settings-payment-gateways-icons.php
@@ -9,12 +9,13 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-$settings = array();
-$settings[] = array(
-	'title' => __( 'Options', 'woocommerce-jetpack' ),
-	'type'  => 'title',
-	'desc'  => __( 'If you want to show an image next to the gateway\'s name on the frontend, enter a URL to an image.', 'woocommerce-jetpack' ),
-	'id'    => 'wcj_payment_gateways_icons_options'
+$settings = array(
+	array(
+		'title'    => __( 'Options', 'woocommerce-jetpack' ),
+		'type'     => 'title',
+		'desc'     => __( 'If you want to show an image next to the gateway\'s name on the frontend, enter a URL to an image.', 'woocommerce-jetpack' ),
+		'id'       => 'wcj_payment_gateways_icons_options',
+	),
 );
 $available_gateways = WC()->payment_gateways->payment_gateways();
 foreach ( $available_gateways as $key => $gateway ) {
@@ -28,28 +29,32 @@ foreach ( $available_gateways as $key => $gateway ) {
 	}
 	$current_icon_url = get_option( 'wcj_gateways_icons_' . $key . '_icon', '' );
 	$desc = ( '' != $current_icon_url ) ? '<img width="16" src="' . $current_icon_url . '" alt="' . $gateway->title . '" title="' . $gateway->title . '" />' : '';
-	$settings[] = array(
-		'title'     => $gateway->title,
-		'desc_tip'  => __( 'Leave blank to set WooCommerce default value', 'woocommerce-jetpack' ),
-		'desc'      => ( '' != $desc_tip ) ? $desc_tip : $desc,
-		'id'        => 'wcj_gateways_icons_' . $key . '_icon',
-		'default'   => '',
-		'type'      => 'text',
-		'css'       => 'min-width:300px;width:50%;',
-		'custom_attributes' => $custom_attributes,
-	);
-	$settings[] = array(
-		'title'     => '',
-		'desc_tip'  => $desc_tip,
-		'desc'      => __( 'Remove Icon', 'woocommerce-jetpack' ),
-		'id'        => 'wcj_gateways_icons_' . $key . '_icon_remove',
-		'default'   => 'no',
-		'type'      => 'checkbox',
-		'custom_attributes' => $custom_attributes,
-	);
+	$settings = array_merge( $settings, array(
+		array(
+			'title'    => $gateway->title,
+			'desc_tip' => __( 'Leave blank to set WooCommerce default value', 'woocommerce-jetpack' ),
+			'desc'     => ( '' != $desc_tip ) ? $desc_tip : $desc,
+			'id'       => 'wcj_gateways_icons_' . $key . '_icon',
+			'default'  => '',
+			'type'     => 'text',
+			'css'      => 'min-width:300px;width:50%;',
+			'custom_attributes' => $custom_attributes,
+		),
+		array(
+			'title'    => '',
+			'desc_tip' => $desc_tip,
+			'desc'     => __( 'Remove Icon', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_gateways_icons_' . $key . '_icon_remove',
+			'default'  => 'no',
+			'type'     => 'checkbox',
+			'custom_attributes' => $custom_attributes,
+		),
+	) );
 }
-$settings[] = array(
-	'type' => 'sectionend',
-	'id'   => 'wcj_payment_gateways_icons_options'
-);
+$settings = array_merge( $settings, array(
+	array(
+		'type'     => 'sectionend',
+		'id'       => 'wcj_payment_gateways_icons_options',
+	),
+) );
 return $settings;
