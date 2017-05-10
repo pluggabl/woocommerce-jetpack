b298cf841fb2a9b4e645b639fcef8cef5dedb6a3
algoritmika <info@algoritmika.com>
2017-05-10 22:43:59 +0300

    v2.8.0-dev: Dev - Code refactoring - Settings from file - PDF INVOICING & PACKING SLIPS - Header.

--- includes/pdf-invoices/submodules/class-wcj-pdf-invoicing-header.php
+++ includes/pdf-invoices/submodules/class-wcj-pdf-invoicing-header.php
@@ -2,7 +2,7 @@
 /**
  * Booster for WooCommerce - PDF Invoicing - Header
  *
- * @version 2.4.7
+ * @version 2.8.0
  * @author  Algoritmika Ltd.
  */
 
@@ -17,7 +17,7 @@ class WCJ_PDF_Invoicing_Header extends WCJ_Module {
 	 *
 	 * @version 2.3.0
 	 */
-	public function __construct() {
+	function __construct() {
 		$this->id         = 'pdf_invoicing_header';
 		$this->parent_id  = 'pdf_invoicing';
 		$this->short_desc = __( 'Header', 'woocommerce-jetpack' );
@@ -25,109 +25,6 @@ class WCJ_PDF_Invoicing_Header extends WCJ_Module {
 		parent::__construct( 'submodule' );
 	}
 
-	/**
-	 * get_settings.
-	 *
-	 * @version 2.4.7
-	 */
-	function get_settings() {
-		$settings = array();
-		$invoice_types = ( 'yes' === get_option( 'wcj_invoicing_hide_disabled_docs_settings', 'no' ) ) ? wcj_get_enabled_invoice_types() : wcj_get_invoice_types();
-		foreach ( $invoice_types as $invoice_type ) {
-			$settings[] = array(
-				'title' => strtoupper( $invoice_type['desc'] ),
-				'type'  => 'title',
-				'id'    => 'wcj_invoicing_' . $invoice_type['id'] . '_header_options',
-			);
-			$settings = array_merge( $settings, array(
-				array(
-					'title'    => __( 'Enable Header', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_enabled',
-					'default'  => 'yes',
-					'type'     => 'checkbox',
-				),
-				array(
-					'title'    => __( 'Header Image', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_image',
-					'default'  => '',
-					'type'     => 'text',
-					'css'      => 'width:33%;min-width:300px;',
-					'desc'     => __( 'Enter a URL to an image you want to show in the invoice\'s header. Upload your image using the <a href="/wp-admin/media-new.php">media uploader</a>.', 'woocommerce-jetpack' ),
-					'desc_tip' => __( 'Leave blank to disable', 'woocommerce-jetpack' ),
-				),
-				array(
-					'title'    => __( 'Header Image Width in mm', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_image_width_mm',
-					'default'  => 50,
-					'type'     => 'number',
-				),
-				array(
-					'title'    => __( 'Header Title', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_title_text',
-					'default'  => $invoice_type['title'],
-					'type'     => 'text',
-				),
-				array(
-					'title'    => __( 'Header Text', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_text',
-					'default'  => __( 'Company Name', 'woocommerce-jetpack' ),
-					'type'     => 'text',
-				),
-				array(
-					'title'    => __( 'Header Text Color', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_text_color',
-					'default'  => '#cccccc',
-					'type'     => 'color',
-					'css'      => 'width:6em;',
-				),
-				array(
-					'title'    => __( 'Header Line Color', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_line_color',
-					'default'  => '#cccccc',
-					'type'     => 'color',
-					'css'      => 'width:6em;',
-				),
-				array(
-					'title'    => __( 'Header Margin', 'woocommerce-jetpack' ),
-					'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_margin_header',
-					'default'  => 10, // PDF_MARGIN_HEADER
-					'type'     => 'number',
-				),
-			) );
-			$settings[] = array(
-				'type' => 'sectionend',
-				'id'   => 'wcj_invoicing_' . $invoice_type['id'] . '_header_options',
-			);
-		}
-
-		$settings = array_merge( $settings, array(
-			array(
-				'title'     => __( 'PDF Invoicing Header General Options', 'woocommerce-jetpack' ),
-				'type'      => 'title',
-				'id'        => 'wcj_invoicing_general_header_options',
-			),
-			array(
-				'title'     => __( 'Default Images Directory', 'woocommerce-jetpack' ),
-				'desc'      => __( 'Default images directory in TCPDF library (K_PATH_IMAGES).', 'woocommerce-jetpack' ),
-				'desc_tip'  => __( 'Try changing this if you have issues displaying image in header.', 'woocommerce-jetpack' ),
-				'id'        => 'wcj_invoicing_general_header_images_path',
-				'default'   => 'empty',
-				'type'      => 'select',
-				'options'   => array(
-					'empty'         => __( 'Empty', 'woocommerce-jetpack' ),
-					'tcpdf_default' => __( 'TCPDF Default', 'woocommerce-jetpack' ),
-					'abspath'       => __( 'ABSPATH', 'woocommerce-jetpack' ),// . ': ' . ABSPATH,
-					'document_root' => __( 'DOCUMENT_ROOT', 'woocommerce-jetpack' ),// . ': ' . $_SERVER['DOCUMENT_ROOT'],
-				),
-			),
-			array(
-				'type'      => 'sectionend',
-				'id'        => 'wcj_invoicing_general_header_options',
-			),
-		) );
-
-		return $this->add_standard_settings( $settings );
-	}
 }
 
 endif;
---
+++ includes/settings/wcj-settings-pdf-invoicing-header.php
@@ -0,0 +1,105 @@
+<?php
+/**
+ * Booster for WooCommerce - Settings - PDF Invoicing - Header
+ *
+ * @version 2.8.0
+ * @since   2.8.0
+ * @author  Algoritmika Ltd.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+$settings = array();
+$invoice_types = ( 'yes' === get_option( 'wcj_invoicing_hide_disabled_docs_settings', 'no' ) ) ? wcj_get_enabled_invoice_types() : wcj_get_invoice_types();
+foreach ( $invoice_types as $invoice_type ) {
+	$settings = array_merge( $settings, array(
+		array(
+			'title'    => strtoupper( $invoice_type['desc'] ),
+			'type'     => 'title',
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_options',
+		),
+		array(
+			'title'    => __( 'Enable Header', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_enabled',
+			'default'  => 'yes',
+			'type'     => 'checkbox',
+		),
+		array(
+			'title'    => __( 'Header Image', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_image',
+			'default'  => '',
+			'type'     => 'text',
+			'css'      => 'width:33%;min-width:300px;',
+			'desc'     => __( 'Enter a URL to an image you want to show in the invoice\'s header. Upload your image using the <a href="/wp-admin/media-new.php">media uploader</a>.', 'woocommerce-jetpack' ),
+			'desc_tip' => __( 'Leave blank to disable', 'woocommerce-jetpack' ),
+		),
+		array(
+			'title'    => __( 'Header Image Width in mm', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_image_width_mm',
+			'default'  => 50,
+			'type'     => 'number',
+		),
+		array(
+			'title'    => __( 'Header Title', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_title_text',
+			'default'  => $invoice_type['title'],
+			'type'     => 'text',
+		),
+		array(
+			'title'    => __( 'Header Text', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_text',
+			'default'  => __( 'Company Name', 'woocommerce-jetpack' ),
+			'type'     => 'text',
+		),
+		array(
+			'title'    => __( 'Header Text Color', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_text_color',
+			'default'  => '#cccccc',
+			'type'     => 'color',
+			'css'      => 'width:6em;',
+		),
+		array(
+			'title'    => __( 'Header Line Color', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_line_color',
+			'default'  => '#cccccc',
+			'type'     => 'color',
+			'css'      => 'width:6em;',
+		),
+		array(
+			'title'    => __( 'Header Margin', 'woocommerce-jetpack' ),
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_margin_header',
+			'default'  => 10, // PDF_MARGIN_HEADER
+			'type'     => 'number',
+		),
+		array(
+			'type'     => 'sectionend',
+			'id'       => 'wcj_invoicing_' . $invoice_type['id'] . '_header_options',
+		),
+	) );
+}
+$settings = array_merge( $settings, array(
+	array(
+		'title'    => __( 'PDF Invoicing Header General Options', 'woocommerce-jetpack' ),
+		'type'     => 'title',
+		'id'       => 'wcj_invoicing_general_header_options',
+	),
+	array(
+		'title'    => __( 'Default Images Directory', 'woocommerce-jetpack' ),
+		'desc'     => __( 'Default images directory in TCPDF library (K_PATH_IMAGES).', 'woocommerce-jetpack' ),
+		'desc_tip' => __( 'Try changing this if you have issues displaying image in header.', 'woocommerce-jetpack' ),
+		'id'       => 'wcj_invoicing_general_header_images_path',
+		'default'  => 'empty',
+		'type'     => 'select',
+		'options'  => array(
+			'empty'         => __( 'Empty', 'woocommerce-jetpack' ),
+			'tcpdf_default' => __( 'TCPDF Default', 'woocommerce-jetpack' ),
+			'abspath'       => __( 'ABSPATH', 'woocommerce-jetpack' ),       // . ': ' . ABSPATH,
+			'document_root' => __( 'DOCUMENT_ROOT', 'woocommerce-jetpack' ), // . ': ' . $_SERVER['DOCUMENT_ROOT'],
+		),
+	),
+	array(
+		'type'     => 'sectionend',
+		'id'       => 'wcj_invoicing_general_header_options',
+	),
+) );
+return $settings;
