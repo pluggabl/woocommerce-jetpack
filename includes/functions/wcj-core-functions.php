c3454e6f072ac5e93f76fca29d419e1fef3128ec
algoritmika <info@algoritmika.com>
2017-07-01 20:48:41 +0300

    v2.9.0-dev: Minor changes.

--- includes/functions/wcj-core-functions.php
+++ includes/functions/wcj-core-functions.php
@@ -16,7 +16,7 @@ if ( ! function_exists( 'wcj_plugin_url' ) ) {
 	 * @version 2.3.0
 	 */
 	function wcj_plugin_url() {
-		return untrailingslashit( plugin_dir_url( realpath( dirname( __FILE__ ) . '/..' ) ) ); // return untrailingslashit( plugin_dir_url( __FILE__ ) );
+		return untrailingslashit( plugin_dir_url( realpath( dirname( __FILE__ ) . '/..' ) ) );
 	}
 }
 
@@ -27,7 +27,7 @@ if ( ! function_exists( 'wcj_plugin_path' ) ) {
 	 * @return string
 	 */
 	function wcj_plugin_path() {
-		return untrailingslashit( realpath( plugin_dir_path( __FILE__ ) . '/../..' ) ); // return untrailingslashit( realpath( plugin_dir_path( __FILE__ ) ) );
+		return untrailingslashit( realpath( plugin_dir_path( __FILE__ ) . '/../..' ) );
 	}
 }
 
