845173a6fa0161155c69ad65927b5414ba2cfb09
algoritmika <info@algoritmika.com>
2017-07-02 14:52:38 +0300

    v2.9.0-dev: Clean up.

--- woocommerce-jetpack.php
+++ woocommerce-jetpack.php
@@ -55,7 +55,7 @@ final class WC_Jetpack {
 	 * @var   string
 	 * @since 2.4.7
 	 */
-	public $version = '2.9.0-dev-201707021449';
+	public $version = '2.9.0-dev-201707021452';
 
 	/**
 	 * @var WC_Jetpack The single instance of the class
